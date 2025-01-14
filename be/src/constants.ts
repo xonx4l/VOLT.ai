07822744da6cec6caba6f6bfabc2dd2361decf24
xonx4l <sk797119@gmail.com>
2025-01-14 23:33:14 +0530

    added files

---
+++ be/src/constants.ts
@@ -0,0 +1,4 @@
+export const  WORK_DIR_NAME = 'project';
+export const  WORK_DIR = `/home/${WORK_DIR_NAME}`;
+export const  MODIFICATIONS_TAG_NAME = "volt_file_modfications";
+
---
+++ be/src/prompts.ts
@@ -0,0 +1,5 @@
+import  { stripIndents, MODIFICATION_TAG_NAME, WORK_DIR, allowedHTMLElements} from './constants';
+
+export const getSystemPrompt = () => {
+    
+}
