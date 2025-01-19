bf2b972dd940afe4f91fafbeb7b84a4ca7001ef3
xonx4l <sk797119@gmail.com>
2025-01-19 07:08:01 +0530

    update index.ts

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,35 +1,18 @@
+require("dotenv").config();
+import express from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import fetch from 'node-fetch';
-import { TextDecoder } from 'util';
 import {getSystemPrompt} from "./prompts";
 
-// If running in Node.js environment, we need to set up global fetch
-if (typeof global !== "undefined") {
-  // @ts-ignore
-  if (!global.fetch) {
-    // @ts-ignore
-    global.fetch = fetch;
-  }
-}
 
 const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const app = express();
+app.use(express.json())
 
-async function streamMessage() {
-  const prompt = "Explain how AI works";
 
-  const system =  getSystemPrompt();
+async function main () {
 
-  try {
-    const result = await model.generateContentStream(prompt);
-    
-    // Use the stream directly from the response
-    for await (const chunk of result.stream) {
-      process.stdout.write(chunk.text());
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
 }
 
-streamMessage();
+main();
+
